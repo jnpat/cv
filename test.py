a30f623409076f550143efc22970ab7bcc463ac6
jnpat <35354784+jnpat@users.noreply.github.com>
2020-11-18 13:34:12 +0700

    Add files via upload

--- test.py
+++ test.py
@@ -2,12 +2,18 @@ import cv2
 
 videoCaptureObject = cv2.VideoCapture(0)
 result = True
-while(result):
+i=20
+while(True):
     ret,frame = videoCaptureObject.read()
     cv2.imshow('camera',frame) #uint8
-    cv2.imwrite("NewPicture.jpg",frame)
-    if cv2.waitKey(10):
+    cv2.imwrite("im/NewPicture.jpg",frame)
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        cv2.imwrite("imm/i"+str(i)+".jpg",frame)
+        if i >= 0:
+            i -=1;
+        else:
+            break
+    elif cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    result = False
 videoCaptureObject.release()
 cv2.destroyAllWindows()
